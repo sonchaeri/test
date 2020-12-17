151df1ae8fa4ed924ba067097bc264debea06231
sonchaeri <50312356+sonchaeri@users.noreply.github.com>
2020-12-18 00:03:02 +0900

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+package com.korea.first;
+
+//클래스 바깥에 쓸 수 있는 키워드는 package, import 밖에 없다.
+//즉, 메소드,변수는 무조건 클래스 안에 있어야한다.
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+
+}
+
+/*
+ * int a = 10;
+ * public static void main(String[] args){
+}
+--> 이렇게 작성 X
+*/
